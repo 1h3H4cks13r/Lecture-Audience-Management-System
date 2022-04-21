e0c812bd8c3b247605e845c14f7fa0fc2c553f4c
Charan Kumar <charankumar.arul@gmail.com>
2022-04-21 14:02:07 +0530

    Configured class functions and entry function

--- atdmgmt.cpp
+++ atdmgmt.cpp
@@ -1,33 +1,58 @@
 #include<iostream>
 #include<fstream>
 
-//using namespace std;
+using namespace std;
 
 class stud
 {
 public:
-	char name[50], pa;
-	int team, std;
+	char att;
+    string name, cls, regno;
 	void add();
 	void attend();
 	void dattend();
 	void disp();
-};
+} s[50];
 
 void stud::add()
-{}
+{
+    cout<<"Enter Name : ";
+    cin >> name;
+    cout<<"Enter Register Number : ";
+    cin >> regno;
+    cout<<"Enter Your Class : ";
+    cin >> cls;
+    att = '\\';
+}
 
 void stud::attend()
-{}
+{
+    cout<<"Name : " <<name<<" Previous Attendance : " <<att<<"\n Enter Today's Attendance : ";
+    cin>>att;
+}
 
 void stud::dattend()
-{}
+{
+    cout<<"Name : " <<name<< "\n Attendance : "<<att;
+}
 
 void stud::disp()
-{}
+{
+    name, cls, regno;
+}
 
 void entry()
-{}
+{
+    int i;
+    char ch='y';
+    for(i=0;i<50,ch!='n';i++)
+    {
+        s[i].add();
+        cout<<"Do you want to enter another student? (y/n) : ";
+        cin>>ch;
+    }
+}
+
 void markat()
 {}
 void dispat()
@@ -39,8 +64,8 @@ void dispdetails()
 int main()
 {
     int choice;
-    std::cout << "\n 1.Students entry\n 2.Mark Attendance\n 3.Display Attendance\n 4.Details\n 5.Exit\n\n\n  Choice? ";
-    std::cin >> choice;
+    cout << "\n 1.Students entry\n 2.Mark Attendance\n 3.Display Attendance\n 4.Details\n 5.Exit\n\n\n  Choice? ";
+    cin >> choice;
     switch(choice)
     {
         case 1:
